014d14bd4170b78496fe105878049aefc9f04dd8
Deena200 <104762591+Deena200@users.noreply.github.com>
2026-06-07 01:12:21 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
 
+import os
+import asyncio
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    print("🤫 Ragasiya Arai Bot Started")
+    print(f"Token Loaded: {'Yes' if TOKEN else 'No'}")
+
+    while True:
+        await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    asyncio.run(main())
